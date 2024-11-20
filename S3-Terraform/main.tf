92c9c8d2a66a8eb08a823b1801a07b3893e17edf
ganasai88 <ganasai88@gmail.com>
2024-11-20 17:07:50 -0500

    terraform apply

--- S3-Terraform/main.tf
+++ S3-Terraform/main.tf
@@ -43,7 +43,9 @@ data "aws_iam_policy_document" "example_policy" {
   statement {
     principals {
       type        = "*"
-      identifiers = ["*"]
+      identifiers = [
+        "arn:aws:iam::767398001187:user/EMR-Cluster",
+        "*"]
     }
 
     actions = [
@@ -53,7 +55,7 @@ data "aws_iam_policy_document" "example_policy" {
     resources = [
       aws_s3_bucket.example.arn,
       "${aws_s3_bucket.example.arn}/*",
-      "*"
+
     ]
   }
 }
